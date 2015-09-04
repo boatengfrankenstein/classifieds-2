0bd51b06c39b67a438c001258bb4c090009f4013
stuckiest <stuckiest@gmail.com>
2015-09-04 11:53:27 -0600

    added new add option

--- app/controllers/classifieds_controller.rb
+++ app/controllers/classifieds_controller.rb
@@ -1,13 +1,34 @@
 class ClassifiedsController < ApplicationController
   def post
-    @classifieds = Classified.all
+    #@classifieds = Classified.all
   end
 
   def index
-    @classified = Classified.find(params[:id])
+    @classifieds = Classified.all
   end
 
   def new
-    @post = Classified.new
+    @classified = Classified.new
   end
+
+  def show
+    @classified = Classified.find(params[:id])
+  end 
+
+  def create
+    @classified = Classified.new(classified_params)
+  
+    if @classified.save
+      redirect_to classifieds_path
+    else
+      render :new
+    end
+  end
+
+   private
+
+  def classified_params
+    params.require(:classified).permit(:title, :author, :body)
+  end
+
 end
